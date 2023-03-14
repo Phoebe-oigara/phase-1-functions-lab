58e1e398013f580c079479f68f335e9dcaadb6a3
Phoebe Oigara <phoebeoigara@gmail.com>
2023-03-14 16:57:19 +0300

    Created four functions calculating various distance parameters.

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks (block) {
+    let headQuarters = 42;
+
+     if (block > 42) {
+    return block - headQuarters;
+}
+    if (block < 42) {
+    return headQuarters - block;
+}
+}
+
+function distanceFromHqInFeet(distance){
+    let blocks = distanceFromHqInBlocks (distance);
+
+    return blocks*264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+
+    const distance = Math.abs(destination - start) * 264;
+    return distance;
+}
+
+function calculatesFarePrice(start, destination) {
+
+      const distance = distanceTravelledInFeet(start, destination);
+      
+        if (distance <= 400) {
+         
+          return 0;
+        } else if (distance > 2500) {
+         
+          return "cannot travel that far";
+        } else if (distance <= 2000) {
+         
+          const fare = (distance - 400) * 0.02;
+          return fare;
+        } else {
+         
+          return 25;
+        }
+      }
+     
+      
